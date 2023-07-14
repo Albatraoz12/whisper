3efaceab61d9d5ce5e42c5a54e8a59076710be5d
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2023-07-14 22:53:38 +0200

    create update logic

--- app/api/auth/user/route.ts
+++ app/api/auth/user/route.ts
@@ -96,6 +96,31 @@ export const GET = async (response: NextResponse) => {
   }
 };
 
+// Update User
+export const PUT = async (request: NextRequest, response: NextResponse) => {
+  try {
+    const body = await request.json();
+    const { username, email, firstName, lastName } = body;
+
+    const newUser = await prisma.user.update({
+      where: { id: '64b151875f95dae1e1a7a864' },
+      data: {
+        username,
+        email,
+        firstName,
+        lastName,
+      },
+    });
+
+    return NextResponse.json(newUser);
+  } catch (error) {
+    console.log(error);
+    return (
+      NextResponse.json({ message: 'POST Error: ', error }), { status: 500 }
+    );
+  }
+};
+
 // Logout User
 export const DELETE = (response: NextResponse) => {
   try {
