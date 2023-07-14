bed4f73e0256c31e492119ea164c115eb2c63a2a
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2023-07-15 01:00:16 +0200

    create api to let user fetch single whisper

---
+++ app/api/public/whisper/[id]/route.ts
@@ -0,0 +1,28 @@
+// URL: /api/public/whisper/:id
+
+import prisma from '@/app/libs/prismaConn';
+import { NextRequest, NextResponse } from 'next/server';
+
+// Get whisper with ID
+export const GET = async (request: NextRequest, { params }: any) => {
+  try {
+    const whisperId = params.id;
+    const whisper = await prisma.whisper.findUnique({
+      where: { id: whisperId },
+    });
+
+    if (!whisper) {
+      return NextResponse.json(
+        { message: "Couldn't find the whisper" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ whisper: whisper }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'An error occurred while retrieving the whisper' },
+      { status: 500 }
+    );
+  }
+};
