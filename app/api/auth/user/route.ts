b96c62bf30479ccbb05493e80ae62ecec6d939ea
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-17 14:15:20 +0200

    redo logic for update user information

--- app/api/auth/user/route.ts
+++ app/api/auth/user/route.ts
@@ -1,13 +1,25 @@
 // URL: /api/user/
 
-import prisma from '@/app/libs/prismaConn';
+import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
+import { authOpstions } from '../[...nextauth]/route';
+import prisma from '@/app/libs/prismaConn';
 
 // Update User
 export const PUT = async (request: NextRequest, response: NextResponse) => {
   try {
     const body = await request.json();
     const { username, email, firstName, lastName } = body;
+    const session = await getServerSession(authOpstions);
+
+    if (!session)
+      return NextResponse.json({ message: 'You need to be authorized' });
+
+    const userId = session?.user.id;
+
+    const findUser = await prisma.user.findUnique({ where: { id: userId } });
+
+    if (!findUser) return NextResponse.json({ message: 'No user found' });
 
     const newUser = await prisma.user.update({
       where: { id: 'userData.id' },
