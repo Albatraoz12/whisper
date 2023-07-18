ce5c5ae7b3ec77b0fdbf3d0c5e801dd7bcc71615
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-18 08:49:41 +0200

    include username whe getting all the whispers

--- app/api/public/whisper/route.ts
+++ app/api/public/whisper/route.ts
@@ -2,10 +2,18 @@
 import prisma from '@/app/libs/prismaConn';
 import { NextResponse } from 'next/server';
 
-// Get all user whispers
+// Get all user whispers with author information (including username)
 export const GET = async (response: NextResponse) => {
   try {
-    const allWhispers = await prisma.whisper.findMany();
+    const allWhispers = await prisma.whisper.findMany({
+      include: {
+        author: {
+          select: {
+            username: true,
+          },
+        },
+      },
+    });
 
     return NextResponse.json({ whispers: allWhispers }, { status: 200 });
   } catch (error) {
