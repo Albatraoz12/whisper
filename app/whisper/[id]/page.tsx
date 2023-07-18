ef72461f1940532d8b0dd582d8fb98451303ad2a
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-18 14:59:20 +0200

    fetch whisper by id and use Whispers component

--- app/whisper/[id]/page.tsx
+++ app/whisper/[id]/page.tsx
@@ -1,8 +1,28 @@
+import Whispers from '@/app/components/Whispers';
 import React from 'react';
 
-const page = ({ params }: any) => {
+const page = async ({ params }: any) => {
   console.log(params.id);
-  return <section>{params.id}</section>;
+  const fetchWhisper = async () => {
+    const res = await fetch(
+      `http://localhost:3001/api/public/whisper/${params.id}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const data = await res.json();
+    console.log('data', data);
+  };
+  const whisper = await fetchWhisper();
+  return (
+    <section>
+      {params.id}
+      <Whispers whisper={whisper} />
+    </section>
+  );
 };
 
 export default page;
