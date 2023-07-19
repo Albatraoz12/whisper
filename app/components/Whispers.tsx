811f390cb8f371239fe9d8c29554e5d88e300e50
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-19 14:56:50 +0200

    install framer-motion and use it on whispers

--- app/components/Whispers.tsx
+++ app/components/Whispers.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Whispers = ({ Whisps }: any) => {
   console.log(Whisps);
@@ -13,13 +14,19 @@ const Whispers = ({ Whisps }: any) => {
     <div className='flex flex-col gap-3 mt-12'>
       {Array.isArray(Whisps) && Whisps.length > 0 ? (
         Whisps.map((whisper: any, index: number) => (
-          <div key={index} className='border-2 rounded p-6 border-white'>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            key={index}
+            className='border-2 rounded p-6 border-white'
+          >
             <a href={`/whisper/${whisper.id}`} className='flex flex-col gap-4'>
               <h2>{whisper.author.username || whisper.author?.name}</h2>
               <p>{whisper.content}</p>
               <p className='self-end'>{formatCreatedAt(whisper.createdAt)}</p>
             </a>
-          </div>
+          </motion.div>
         ))
       ) : (
         <div className='border-2 rounded p-6 border-white'>
