2df42018d2a081311e5d87a7b373ea5cb9c1d4ec
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-19 11:39:17 +0200

    initialize query wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import QueryWrapper from './QueryWrapper';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <main className='w-[90%] container mx-auto'>{children}</main>
+        <QueryWrapper>
+          <main className='w-[90%] container mx-auto'>{children}</main>
+        </QueryWrapper>
       </body>
     </html>
   );
