cefd619ba8322bf084bd35a7995af92ea542e452
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-19 14:17:37 +0200

    send user details to logged component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,7 +14,7 @@ export default async function Navbar() {
       </Link>
       <ul className='flex items-center gap-6'>
         {!session?.user && <Login />}
-        {session?.user && <Logged />}
+        {session?.user && <Logged user={session.user} />}
       </ul>
     </nav>
   );
