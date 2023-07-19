bb60a131d0db45309ee73176d5b7d59dae780b2b
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-19 14:17:46 +0200

    display name correctly

--- app/components/auth/Logged.tsx
+++ app/components/auth/Logged.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { signOut } from 'next-auth/react';
 
-export default function Logged() {
+export default function Logged({ user }: any) {
   return (
     <li className='flex gap-8 items-center'>
+      <p>{user && user.name ? <>{user.name}</> : <>{user.firstName}</>}</p>
       <button
         className='bg-gray-700 text-white text-sm px-6 py-2 rounded-md'
         onClick={() => signOut()}
