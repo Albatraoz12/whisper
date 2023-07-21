d5dfc86b2b49cee5ca59d71d909d6f6159d12f09
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-21 11:35:53 +0200

    fix route to include request first

--- app/api/public/whisper/route.ts
+++ app/api/public/whisper/route.ts
@@ -1,9 +1,9 @@
 // URL: /api/public/whisper
 import prisma from '@/app/libs/prismaConn';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 // Get all user whispers with author information (including username)
-export const GET = async (response: NextResponse) => {
+export const GET = async (request: NextRequest, response: NextResponse) => {
   try {
     const allWhispers = await prisma.whisper.findMany({
       include: {
