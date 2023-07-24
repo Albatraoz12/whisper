f3347e95890bf4d46c8cc71e7cba8e4ed749c6d0
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-24 14:34:12 +0200

    add min height on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${inter.className} max-w-[1000px] mx-auto border-x-slate-200 border-x-2`}
+        className={`${inter.className} max-w-[1000px] min-h-[100vh] mx-auto border-x-slate-200 border-x-2`}
       >
         <QueryWrapper>
           <Navbar />
