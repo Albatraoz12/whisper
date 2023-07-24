a21f937167f0326727a1dd40fbdd8074aff474b6
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-24 08:49:59 +0200

    fetch single whisper with useQuesy

--- app/whisper/[id]/page.tsx
+++ app/whisper/[id]/page.tsx
@@ -1,23 +1,25 @@
+'use client';
 import Whispers from '@/app/components/Whispers';
+import { WhispersTyps } from '@/app/types/Whispers';
+import axios from 'axios';
 import React from 'react';
+import { useQuery } from 'react-query';
 
-const page = async ({ params }: any) => {
-  console.log(params.id);
+const Page = ({ params }: any) => {
   const fetchWhisper = async () => {
-    const res = await fetch(
-      `http://localhost:3001/api/public/whisper/${params.id}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-    const data = await res.json();
+    const res = await axios.get(`/api/public/whisper/${params.id}`);
+    const data = await res.data;
     return data;
   };
-  const whisper = await fetchWhisper();
-  return <section>{whisper && <Whispers Whisps={whisper} />}</section>;
+
+  const { data, error, isLoading } = useQuery<WhispersTyps[]>({
+    queryFn: fetchWhisper,
+    queryKey: ['whisper'],
+  });
+  if (error) return error;
+  if (isLoading) return 'Loading...';
+
+  return <section>{data && <Whispers Whisps={data} />}</section>;
 };
 
-export default page;
+export default Page;
