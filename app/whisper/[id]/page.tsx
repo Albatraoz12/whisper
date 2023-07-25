e183921c33d19df7ddbe1cc016637489ecc2f78c
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-25 08:50:55 +0200

    create a component to send a comment to a whisper

--- app/whisper/[id]/page.tsx
+++ app/whisper/[id]/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import AddComment from '@/app/components/AddComment';
 import Whispers from '@/app/components/Whispers';
 import { WhispersTyps } from '@/app/types/Whispers';
 import axios from 'axios';
@@ -12,14 +13,28 @@ const Page = ({ params }: any) => {
     return data;
   };
 
-  const { data, error, isLoading } = useQuery<WhispersTyps[]>({
+  const { data, error, isLoading } = useQuery<WhispersTyps>({
     queryFn: fetchWhisper,
     queryKey: ['whisper'],
   });
-  if (error) return error;
-  if (isLoading) return 'Loading...';
 
-  return <section>{data && <Whispers Whisps={data} />}</section>;
+  if (error) return <div>Error</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  if (!data) {
+    return <div>No data found.</div>;
+  }
+
+  return (
+    <>
+      <section>
+        <Whispers Whisps={[data]} />
+      </section>
+      <section>
+        <AddComment id={data.id} />
+      </section>
+    </>
+  );
 };
 
 export default Page;
