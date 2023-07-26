d9d528da6fd67fa94064fad7db140bef8dea203b
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-26 08:37:26 +0200

    create a comments component

---
+++ app/components/Comments.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react';
+import { motion } from 'framer-motion';
+import { formatCreatedAt } from '../libs/helpers';
+
+const Comments = ({ comment }: any) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+      className='border-2 rounded p-6 border-white'
+    >
+      <div className='flex flex-col gap-4'>
+        <h2>{comment.author.username || comment.author?.name}</h2>
+        <p>{comment.title}</p>
+        <div className='flex justify-between'>
+          <span>{formatCreatedAt(comment.createdAt)}</span>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Comments;
