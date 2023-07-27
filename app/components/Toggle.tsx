a3249fc5ded25bfff0c12eb3324a9cc31021cc1d
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-27 15:05:50 +0200

    add toggle to confirm deleteing a whisper

---
+++ app/components/Toggle.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+type ToggleProps = {
+  deletePost: () => void;
+  setToggle: (toggle: boolean) => void;
+};
+
+export default function Toggle({ deletePost, setToggle }: ToggleProps) {
+  return (
+    <section
+      onClick={(e) => {
+        setToggle(false);
+      }}
+      className='fixed bg-black/20  w-full h-full z-20 left-0 top-0'
+    >
+      <div className='absolute bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-12 rounded-lg flex flex-col gap-6'>
+        <h2 className='text-xl'>Are you sure you want to delete this post ?</h2>
+        <h3 className='text-red-300 text-sm'>
+          Press this button to delete this post
+        </h3>
+        <button
+          onClick={deletePost}
+          className='bg-red-600 text-sm text-white py-2 px-4 rounded-md'
+        >
+          Delete Post
+        </button>
+      </div>
+    </section>
+  );
+}
--- app/dashboard/EditWhisper.tsx
+++ app/dashboard/EditWhisper.tsx
@@ -1,8 +1,11 @@
 'use client';
 import Image from 'next/image';
-import React from 'react';
-import { useQueryClient } from 'react-query';
+import React, { useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
 import { formatCreatedAt } from '../libs/helpers';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import Toggle from '../components/Toggle';
 
 const EditWhisper = ({
   avatar,
@@ -12,8 +15,36 @@ const EditWhisper = ({
   id,
   createdAt,
 }: any) => {
+  const [toggle, setToggle] = useState(false);
   let deleteToastID: string;
   const queryClient = useQueryClient();
+
+  //Delete posts
+  const { mutate } = useMutation(
+    async (id: string) => {
+      await axios.delete(`/api/auth/whisper/${id}`);
+    },
+    {
+      onError: (error) => {
+        console.log(error);
+        toast.error('Error while deleteing the post, try again later!', {
+          id: deleteToastID,
+        });
+      },
+      onSuccess: (data) => {
+        console.log(data);
+        toast.success('Your post has now been deleted', { id: deleteToastID });
+        queryClient.invalidateQueries(['getAuthWhispers']);
+      },
+    }
+  );
+
+  const deletePost = () => {
+    deleteToastID = toast.loading('Deleting your post...', {
+      id: deleteToastID,
+    });
+    mutate(id);
+  };
 
   return (
     <>
@@ -31,7 +62,14 @@ const EditWhisper = ({
             <span>{formatCreatedAt(createdAt)}</span>
           </div>
         </a>
+        <button
+          onClick={() => setToggle(true)}
+          className='text-sm font-bold text-red-500'
+        >
+          Delete Post
+        </button>
       </section>
+      {toggle && <Toggle deletePost={deletePost} setToggle={setToggle} />}
     </>
   );
 };
