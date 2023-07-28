dc4ff156610aff70b3d6dbf581cf3eb89ec5be17
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-28 11:52:57 +0200

    change background for toggle component

--- app/components/Toggle.tsx
+++ app/components/Toggle.tsx
@@ -13,7 +13,7 @@ export default function Toggle({ deletePost, setToggle }: ToggleProps) {
       }}
       className='fixed bg-black/20  w-full h-full z-20 left-0 top-0'
     >
-      <div className='absolute bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-12 rounded-lg flex flex-col gap-6'>
+      <div className='absolute bg-slate-500 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-12 rounded-lg flex flex-col gap-6'>
         <h2 className='text-xl'>Are you sure you want to delete this post ?</h2>
         <h3 className='text-red-300 text-sm'>
           Press this button to delete this post
