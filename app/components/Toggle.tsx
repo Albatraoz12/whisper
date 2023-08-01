030d3011983faabf1eaaa18ecd0b3c3f387aeb6c
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-08-01 08:50:43 +0200

    fix styling for mobile view for delete toggle

--- app/components/Toggle.tsx
+++ app/components/Toggle.tsx
@@ -11,9 +11,9 @@ export default function Toggle({ deletePost, setToggle }: ToggleProps) {
       onClick={(e) => {
         setToggle(false);
       }}
-      className='fixed bg-black/20  w-full h-full z-20 left-0 top-0'
+      className='fixed bg-black/20 w-full h-full z-20 left-0 top-0'
     >
-      <div className='absolute bg-slate-500 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-12 rounded-lg flex flex-col gap-6'>
+      <div className='absolute bg-slate-500 w-[90%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-12 rounded-lg flex flex-col gap-6'>
         <h2 className='text-xl'>Are you sure you want to delete this post ?</h2>
         <h3 className='text-red-300 text-sm'>
           Press this button to delete this post
