7eaccff8d4b72dd1e85a1f14e2348eb881d97aee
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-08-04 13:31:59 +0200

    make the buttons functionall

--- app/components/auth/admin/Admin.tsx
+++ app/components/auth/admin/Admin.tsx
@@ -1,17 +1,44 @@
-'use client'
-import React, {useState} from 'react'
-import RegisterUser from './RegisterUser'
+'use client';
+import React, { useState } from 'react';
+import RegisterUser from './RegisterUser';
+import MyWhispers from '@/app/dashboard/MyWhispers';
 
 const Admin = () => {
-    const [admin, setAdmin] = useState(false)
+  const [admin, setAdmin] = useState(false);
+  const [user, setUser] = useState(false);
   return (
     <>
-    <div>
-        <button onClick={() => setAdmin(true)}>Admin</button>
-    </div>
-    {admin && <RegisterUser />}
-</>
-  )
-}
+      <div className='flex justify-center items-center gap-10 mt-5 mb-10'>
+        <button
+          className={
+            user
+              ? 'bg-blue-500 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:bg-blue-500'
+              : 'bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+          }
+          onClick={() => {
+            setAdmin(false);
+            setUser(true);
+          }}
+        >
+          User Panel
+        </button>
+        <button
+          className={
+            admin
+              ? 'bg-blue-500 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:bg-blue-500'
+              : 'bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+          }
+          onClick={() => {
+            setUser(false);
+            setAdmin(true);
+          }}
+        >
+          Admin Panel
+        </button>
+      </div>
+      {admin ? <RegisterUser /> : <MyWhispers />}
+    </>
+  );
+};
 
-export default Admin
+export default Admin;
