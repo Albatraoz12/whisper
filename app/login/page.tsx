34a585e1b099a3cf84ceb9c27ab7002e7d4bfdb0
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-08-04 15:02:47 +0200

    style the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { signIn } from 'next-auth/react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { FormEvent } from 'react';
 
@@ -18,32 +19,50 @@ export default function Login() {
     <div className='container mx-auto flex flex-col justify-center items-center'>
       <form
         onSubmit={handleSubmit}
-        className='container mx-auto my-5 flex flex-col justify-evenly px-4'
+        className='container mx-auto my-5 flex flex-col gap-3 justify-evenly px-4'
       >
-        <h2>Login</h2>
-        <label htmlFor='email'>Username:</label>
-        <input
-          type='email'
-          id='email'
-          name='email'
-          className='text-black p-1 outline-none'
-          required
-        />
-        <label htmlFor='password'>Password:</label>
-        <input
-          type='password'
-          id='password'
-          name='password'
-          className='text-black p-1 outline-none'
-          required
-        />
-        <button type='submit'>Submit</button>
+        <h2 className='text-center'>Login</h2>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            className='text-black p-1 outline-none py-2 rounded'
+            required
+          />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            className='text-black p-1 outline-none py-2 rounded'
+            required
+          />
+        </div>
+        <button
+          className=' my-3 bg-blue-500 hover:bg-blue-700 max-w-[100px] self-center text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+          type='submit'
+        >
+          Submit
+        </button>
       </form>
       <p>
         Not registered yet? <Link href='/register'>Register here</Link>
       </p>
+      <span className='max-w-[80%] w-full border-t-2 border-white my-4'></span>
 
-      <button onClick={() => signIn('google')}> Login with google</button>
+      <button className='flex gap-3' onClick={() => signIn('google')}>
+        <Image
+          src={'https://freesvg.org/img/1534129544.png'}
+          height={25}
+          width={25}
+          alt={'google icon'}
+        />
+        Login with google
+      </button>
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.dicebear.com', 'lh3.googleusercontent.com'],
+    domains: [
+      'avatars.dicebear.com',
+      'lh3.googleusercontent.com',
+      'https://freesvg.org',
+      'freesvg.org',
+    ],
   },
 };
 
