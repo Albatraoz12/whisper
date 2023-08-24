390963beced011e2a05ef43f908e33100f943063
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-08-24 13:18:37 +0200

    refetch whisper when user liked to set live reload effect

--- app/components/Heart.tsx
+++ app/components/Heart.tsx
@@ -1,24 +1,44 @@
 'use client';
-import axios from 'axios';
+
 import Image from 'next/image';
+import axios, { AxiosError } from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
+import toast from 'react-hot-toast';
 
 const Heart = ({ id, likes, userId }: any) => {
   const [hasLiked, setHasLiked] = useState(false);
+  const queryClient = useQueryClient();
+  let commentToastId: string;
+
+  const { mutate } = useMutation(
+    async () => {
+      return axios.post(`/api/auth/whisper/like/${id}`);
+    },
+    {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(['whisper']);
+        toast.success('You have liked this whisper', { id: commentToastId });
+      },
+      onError: (error) => {
+        if (error instanceof AxiosError) {
+          toast.error(error?.response?.data.message, { id: commentToastId });
+        }
+      },
+    }
+  );
 
   useEffect(() => {
     const userHasLiked = likes.some((like: any) => like === userId);
     setHasLiked(userHasLiked);
   }, [likes, userId]);
   console.log(hasLiked);
-  const like = async () => {
-    try {
-      const response = await axios.post(`/api/auth/whisper/like/${id}`);
-      const data = await response.data;
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
+
+  const like = () => {
+    commentToastId = toast.loading('Adding your comment', {
+      id: commentToastId,
+    });
+    mutate();
   };
 
   return (
